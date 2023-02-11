9d146bd28ff25693ed8c872a9d78a86946e0d55f
Thijmeee <t.franken1@student.avans.nl>
2023-02-11 15:03:23 +0100

    Opdracht 20.1 - Week 2

---
+++ Week2/Opdracht_20_1.java
@@ -0,0 +1,28 @@
+package Week2;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Opdracht_20_1 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList = new ArrayList<>();
+        try {
+            File file = new File("C:\\Users\\thijm\\OneDrive - Avans Hogeschool\\Documenten\\GitHub\\OOSD-huiswerk\\Week2\\opdracht20.1.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNext()){
+                arrayList.add(scanner.next());
+            }
+
+            Collections.sort(arrayList);
+            Collections.reverse(arrayList);
+            arrayList.forEach(System.out::println);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
