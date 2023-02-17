2c9ccb539eeef2d23206c69b58fbbb017ccc3b25
Thijmeee <t.franken1@student.avans.nl>
2023-02-17 12:00:17 +0100

    Testen met JSON

---
+++ Week3/JSON/TestingJson.java
@@ -0,0 +1,54 @@
+package Week3.JSON;
+
+import javax.json.*;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+public class TestingJson {
+    public static void main(String[] args) throws Exception {
+        JsonObject jsonObject = Json.createObjectBuilder()
+                .add("naam", "Jan Jansen")
+                .add("leeftijd", 25)
+                .add("adres", "Kerkstraat 123").build();
+
+        try (FileWriter fileWriter = new FileWriter("output.json")) {
+            JsonWriter jsonWriter = Json.createWriter(fileWriter);
+            jsonWriter.writeObject(jsonObject);
+            jsonWriter.close();
+            System.out.println("Het JSON-object is succesvol geschreven naar het bestand.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        readJSON();
+
+    }
+
+    public static void readJSON(){
+        String url = "https://api.spacexdata.com/v3/launches/";
+
+        JsonArray data = Json.createReader(callUrl(url)).readArray();
+
+        for (int i = 0; i < data.size(); i++) {
+            JsonObject rocket = data.getJsonObject(i).getJsonObject("rocket");
+            System.out.println(rocket.getJsonString("rocket_id"));
+        }
+    }
+
+    public static InputStream callUrl(String url) {
+        try {
+            HttpsURLConnection connection = (HttpsURLConnection) new URL(url).openConnection();
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setRequestProperty("Thijme", "Thijme is aan het testen");
+            connection.setDoOutput(true);
+            return connection.getInputStream();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+
