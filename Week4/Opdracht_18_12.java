c9ed1643739e00b6396bc7f681531197e5fe3de4
Thijmeee <t.franken1@student.avans.nl>
2023-03-05 13:26:39 +0100

    Opdracht 18.12

--- Week4/Opdracht_18_12.java
+++ Week4/Opdracht_18_12.java
@@ -5,15 +5,20 @@ public class Opdracht_18_12 {
     public static String newWord = "";
 
     public static void main(String[] args) {
-        reverseDisplay("lekker", 4);
+        reverseDisplay("lekker", 0);
 
     }
 
-    public static void reverseDisplay(String value, int high){
-        if (high != 0){
-            newWord = value.charAt(value.length() - 1) + "";
-            value = value.substring(0, value.length() - 1);
-            System.out.println(newWord);
+    public static void reverseDisplay(String value) {
+        if (!value.isEmpty()) {
+            System.out.print(value.charAt(value.length() - 1));
+            reverseDisplay(value.substring(0, value.length() - 1));
+        }
+    }
+
+    public static void reverseDisplay(String value, int high) {
+        if (high >= 0) {
+            System.out.print(value.charAt(high));
             reverseDisplay(value, high - 1);
         }
     }
